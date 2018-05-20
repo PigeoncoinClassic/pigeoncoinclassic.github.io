7aac996253dd4f171152f8a0689eaa73c5f84a16
Luke Pighetti <lukepighetti@gmail.com>
2018-05-20 13:11:44 -0400

    graph works

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 // global state object
 var state = {}
 
-// var myChart is defined in the body of chart.html
+// var statsGraph is defined in the body of chart.html
 
 // firebase config
 const config = {
@@ -36,6 +36,8 @@ onReady(()=>{
   // set state to localStorage state
   if(localStorage.state){
     setState(JSON.parse(localStorage.state))
+  }else{
+    // todo set default state for widget and roadmap menu
   }
 
 })
@@ -44,18 +46,21 @@ onReady(()=>{
 
 // listeners
 
-//    state change
-//      update widgets
-//      update graph
 //      update roadmap menu
-//      update progress bars
 
 function stateListener(){
   // new state!
   console.log(state)
 
+  // update widgets
   updateAllWidgets()
   activateWidget(state.graphState)
+
+  // update graph
+  if(typeof statsGraph !== 'undefined') updateGraph()
+  
+  // update progress bars
+  updateMiningPool()
 
 }
 
@@ -90,15 +95,17 @@ function historyDataListener(db){
 // updaters
 
 function updateAllWidgets(){
+  const {chain, market } = state.latestData
+
   // update all widgets with proper data format
-  updateWidget('chain-hashrate', +(state.latestData.chain.hashrate / 1e9).toPrecision(2) + ' GH')
-  updateWidget('chain-difficulty', +state.latestData.chain.difficulty.toPrecision(3) )
-  updateWidget('chain-blockTime', (state.latestData.chain.blockTime / 60).toFixed(1) + ' min' )
-  updateWidget('chain-retarget', state.latestData.chain.retarget)
-  updateWidget('market-priceBtc', Math.round(state.latestData.market.priceBtc * 1e8) + ' sats')
-  updateWidget('market-volumeBtc', state.latestData.market.volumeBtc.toFixed(1) + ' BTC')
-  updateWidget('market-marketCapBtc', Math.round(state.latestData.market.marketCapBtc) + ' BTC')
-  updateWidget('chain-supply', +(state.latestData.chain.supply / 1e6).toPrecision(2) + 'M PGN')
+  updateWidget('chain-hashrate', +(chain.hashrate / 1e9).toPrecision(2) + ' GH')
+  updateWidget('chain-difficulty', +chain.difficulty.toPrecision(3) )
+  updateWidget('chain-blockTime', (chain.blockTime / 60).toFixed(1) + ' min' )
+  updateWidget('chain-retarget', 2016 - chain.height % 2016 + ' blocks')
+  updateWidget('market-priceBtc', Math.round(market.priceBtc * 1e8) + ' sats')
+  updateWidget('market-volumeBtc', market.volumeBtc.toFixed(1) + ' BTC')
+  updateWidget('market-marketCapBtc', Math.round(market.marketCapBtc) + ' BTC')
+  updateWidget('chain-supply', +(chain.supply / 1e6).toPrecision(2) + 'M PGN')
 
   function updateWidget(dataId, newData){
     // find a widget by dataId, give it newData!
@@ -119,21 +126,95 @@ function activateWidget(dataId){
     const els = document.querySelectorAll(`#graph-widgets .notification`)
     Array.from(els).forEach(el => {
       // remove class is-primary
-      el.classList.remove('is-primary')
+      if(el) el.classList.remove('is-primary')
     })
   }
 
   function highlightWidget(dataId){
     // find dataId, add class id-primary
     const el = document.querySelector(`#graph-widgets [data-id="${dataId}"]`)
-    el.classList.add('is-primary')
+    if(el) el.classList.add('is-primary')
   }
 }
 
-//    update graph
+function updateMiningPool(){
+  const { pool } = state.latestData
 
-//      highlight widget
+  // update pool-miners
+  updateProgressBar('pool-miners', pool.miners, 1000)
+  updateTag('pool-miners-tag', Math.round(pool.miners))
 
+  // update pool-payout
+  const lastPayout = Date.now()/1000/60-60
+  updateProgressBar('pool-payout', lastPayout % 180, 180)
+  updateTag('pool-payout-tag', `in ${Math.round(180 - lastPayout % 180)}m`)
+
+  // update pool-lastBlock
+  const minutesAgo = (Date.now()/1000 - pool.lastBlockTime)/60
+  updateProgressBar('pool-lastBlock', minutesAgo, pool.timeToFind/60 * 2)
+  updateTag('pool-lastBlock-tag', `${Math.round(minutesAgo)}m ago`)
+
+
+  function updateTag(dataId, value){
+    const el = document.querySelector(`[data-id="${dataId}"]`)
+    if(el) el.innerHTML = value
+  }
+
+  function updateProgressBar(dataId, value, max){
+    const el = document.querySelector(`[data-id="${dataId}"]`)
+    if(el){
+      el.value = value
+      el.max = max
+    }
+  }
+}
+
+function updateGraph(){
+  const chart = statsGraph // defined in the body of chart.html
+  const { historyData , graphState} = state
+  const dataId = graphState
+  let k = 1 // we use this to scale our data
+
+  // scale our data so it displays right
+  if(dataId == 'chain-hashrate') k = 1e-9
+  if(dataId == 'chain-blockTime') k = 1/60
+  if(dataId == 'chain-supply') k = 1e-6
+  if(dataId == 'market-priceBtc') k = 1e8
+
+  const {data, labels} = parseHistoryData(historyData, dataId, k)
+
+  pushUpdateToGraph(chart, data, labels)
+
+
+  function parseHistoryData(historyData, dataId, k){
+    // get parentId and childId from dataId
+    const [ parentId, childId ] = dataId.split('-')
+
+    const data = []
+    const labels = []
+
+    for(dataPoint of Object.values(historyData)){
+      // special case for retarget
+      if(dataId === 'chain-retarget'){
+        data.push(2016 - dataPoint[parentId]['height'] % 2016)
+      }else{
+        // push the data
+        data.push(dataPoint[parentId][childId] * k)
+      }
+      // push the label
+      labels.push(dataPoint[parentId]['timestamp'] * 1000)
+    }
+
+    console.log({ data, labels })
+    return { data, labels }
+  }
+
+  function pushUpdateToGraph(chart, data, labels){
+    chart.data.labels = labels
+    chart.data.datasets[0].data = data
+    chart.update();
+  }
+}
 //    update roadmap
 //      hide all visible items .is-hidden-desktop
 //      show matching item
